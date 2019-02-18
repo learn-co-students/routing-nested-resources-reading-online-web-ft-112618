b72e3e3a4e97d80192fc0fe6c7d64c99c2d6b520
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-18 09:41:54 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,15 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  # def posts_index
+  #   @author = Author.find_by_id(params[:id])
+  #   @posts = @author.posts
+  #   render template: 'posts/index'
+  # end
+  #
+  # def post
+  #   @author = Author.find_by_id(params[:id])
+  #   @post = Post.find_by_id(params[:post_id])
+  #   render template: 'posts/show'
+  # end
 end
